b0ee520f2d981e0b8aae66974ce7dc5cd74f2bd9
Carlos Hernández <Calichi15@hotmail.com>
2023-03-22 00:08:06 -0500

    Estado estable

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const pages = document.querySelectorAll(".page");
+let state = true;
+let deg = 180;
 
 pages.forEach((page, index) => {
   page.style.zIndex = pages.length - index;
-  
+  page.addEventListener('click', function() {
+    //this.classList.toggle('flipped');
+    state = !state;
+    let front = page.querySelector('.card-front');
+    let back = page.querySelector('.card-back');
+
+    front.style.transform = `rotateY(${deg * !!state}deg)`;
+    back.style.transform = `rotateY(${deg * Number(!state)}deg)`;
+  });
 });
 
 //const card = document.getElementById('card');
