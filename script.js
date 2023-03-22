d93e25474ee88a2477c27690d0f1ab5eae6f5b3f
Carlos Hernández <Calichi15@hotmail.com>
2023-03-21 23:29:50 -0500

    primera fase de prueba

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const pages = document.querySelectorAll(".page");
 
 pages.forEach((page, index) => {
   page.style.zIndex = pages.length - index;
+  
 });
+
+//const card = document.getElementById('card');
+
